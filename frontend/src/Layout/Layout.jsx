3281c7500b2d11b23ab784dcb9cf269a108d3a5a
samitdhiman027 <samitdhiman0001@gmail.com>
2025-02-06 10:34:51 +0530

    home page update

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+import { Routes , Route } from 'react-router-dom'
 
+import Home from '../Pages/Home/Home'
 const Layout = () => {
   return (
     <>
-   <Routes>
-
+    <Navbar/>
     
+   <Routes>
+        <Route index element={<Home/>}/>
+
    </Routes>
     </>
   )
