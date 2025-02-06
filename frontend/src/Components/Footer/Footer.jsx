4289deb1e987b541bd923b24d7ca2802070dbf78
samitdhiman027 <samitdhiman0001@gmail.com>
2025-02-06 11:50:07 +0530

    set header

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <footer>
+        <div className="inner-footer">
+            
+        </div>
+     </footer>
+    </>
+  )
+}
+
+export default Footer
