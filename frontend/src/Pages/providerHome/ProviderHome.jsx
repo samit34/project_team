2c684db0d0afae49c7f85cd3bacb1fce91d4b3ec
kaursehaj01 <sehajk.001@gmail>
2025-02-20 09:20:08 +0530

    sss

--- frontend/src/Pages/providerHome/ProviderHome.jsx
+++ frontend/src/Pages/providerHome/ProviderHome.jsx
@@ -5,6 +5,8 @@ import Related_jobs from './Providerhome-com/Related-jobs/Related_jobs'
 import High_rank_user from './Providerhome-com/high_rank-user/High_rank_user'
 import Footer from '../../Components/Footer/Footer'
 import ServiceStatic from './Providerhome-com/serviceStatic/ServiceStatic'
+import ProvAdd from './Providerhome-com/provAdd/ProvAdd'
+import ProvGrid from './Providerhome-com/provGrid/ProvGrid'
 
 
 
@@ -16,6 +18,8 @@ const ProviderHome = () => {
         <Related_jobs/>
         <ServiceStatic/>
         <High_rank_user/>
+        <ProvGrid/>
+        <ProvAdd/>
 
         <Footer/>
   </>
---
+++ frontend/src/Pages/providerHome/Providerhome-com/provAdd/ProvAdd.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const ProvAdd = () => {
+    return (
+        <>
+            {/* <section className="provadd-section bg-red-100">
+                <div className="provadd-container">
+                    <div className="max-w-6xl mx-auto p-6 ">
+                        <div className="flex flex-col sm:flex-row gap-4 justify-center align-items-center">
+                            <div className="w-1/3">
+                                <div className="rounded-lg shadow-md  bg-white p-2">
+                                   <img src="./assets/img.jpg" alt="" className='w-full'/>
+                                </div>
+
+                            </div>
+                            <div className="w-2/3">
+                                <div className="rounded-lg shadow-md bg-white p-2">
+                                 <h1>title</h1>
+                                 <h5>heading h5</h5>
+                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, vero.</p>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-2 md:grid-cols-2">
+                            <div className="w-3/4">
+                                <div className="rounded-lg shadow-md bg-white p-2">
+                               fdffdfd
+                                </div>
+
+                            </div>
+                            <div className="w-1/4">
+                                <div className="rounded-lg shadow-md bg-white p-2">
+                                    ffff
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                       
+                    </div>
+
+
+                </div>
+            </section> */}
+        </>
+    )
+}
+
+export default ProvAdd
