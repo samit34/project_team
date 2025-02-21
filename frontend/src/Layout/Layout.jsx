988084be494f8ca870d9707dae4bfe3bf3c4fe5f
samitdhiman027 <samitdhiman0001@gmail.com>
2025-02-21 15:32:39 +0530

    awe push

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -1,16 +1,4 @@
 
-// import React from 'react'
-// import Navbar from '../Components/Navbar/Navbar'
-// import { Routes , Route } from 'react-router-dom'
-// import './Layout.css'
-// import Home from '../Pages/Home/Home'
-// import ProviderHome from '../Pages/providerHome/ProviderHome'
-// import ProviderDashboard from '../Pages/providerDashboard/ProviderDashboard'
-// import ConsumerDashbord from '../Pages/consumerDashboard/ConsumerDashbord'
-// import Showjobs from '../Pages/providerDashboard/Showjobs/Showjobs'
-// import Stacks from '../Pages/providerDashboard/Stackpage/Stacks'
-// import Jobpost from '../Pages/consumerDashboard/jobpost/Jobpost'
-// import ProvProfile from '../Pages/providerDashboard/provProfile/ProvProfile'
 
 
 
@@ -28,7 +16,7 @@ import Stacks from '../Pages/providerDashboard/Stackpage/Stacks';
 import Jobpost from '../Pages/consumerDashboard/jobpost/Jobpost';
 import ProvProfile from '../Pages/providerDashboard/provProfile/ProvProfile';
 import Dashboard from '../Pages/Dashboard';
-import '../css/style.css'
+// import '../css/style.css'
 
 const Layout = () => {
 
