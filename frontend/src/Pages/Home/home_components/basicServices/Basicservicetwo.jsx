1393355606496335a17132815d58b1b7800134dd
samitdhiman027 <samitdhiman0001@gmail.com>
2025-03-11 16:26:36 +0530

    Basic service section done

--- frontend/src/Pages/Home/home_components/basicServices/Basicservicetwo.jsx
+++ frontend/src/Pages/Home/home_components/basicServices/Basicservicetwo.jsx
@@ -4,165 +4,173 @@ import { SiBookstack } from "react-icons/si";
 import { IoIosWater } from "react-icons/io";
 import { FaHome } from "react-icons/fa";
 import { FaCar } from "react-icons/fa";
-
+import { MdOutlinePets } from "react-icons/md";
+import { TbRibbonHealth } from "react-icons/tb";
+import { FaBusinessTime } from "react-icons/fa";
+import { FaChildren } from "react-icons/fa6";
+import { FaBuilding } from "react-icons/fa";
+import { MdSecurity } from "react-icons/md";
+import { FaComputer } from "react-icons/fa6";
+import { FcMoneyTransfer } from "react-icons/fc";
+import { MdCleaningServices } from "react-icons/md";
+import { GiMountaintop } from "react-icons/gi";
+import { GrYoga } from "react-icons/gr";
+import { MdFamilyRestroom } from "react-icons/md";
+import { MdOutlineFaceRetouchingNatural } from "react-icons/md";
 const Basicservicetwo = () => {
   return (
     <>
     <h1 className="text-center text-4xl font-bold"  >Basic-Services</h1>
-      <div className=" mx-auto p-6">
+      <div className=" container max-w-6xl mx-auto p-6">
         <div className="basic-service-div">
           <div className="basic-service-inner-div flex flex-wrap gap-10 ">
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
                 <FaPlateWheat className="text-red-800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Restaurants</h5>
             </div>
 
             {/* service-two-book-services */}
-            <div className="service-one">
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
                 < SiBookstack className="text-green-800 text-4xl " />
               </div>
-              <h5>Education</h5>
+              <h5   className= "text-center">Education</h5>
             </div>
 
             {/* service-three plumber-service */}
-            <div className="service-one">
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
                 <IoIosWater className="text-blue-800 text-4xl " />
               </div>
-              <h5>Plumbing</h5>
+              <h5   className= "text-center">Plumbing</h5>
             </div>
 
-            {/* service-one home service */}
-            <div className="service-one">
+            {/* service-one    w-20 home service */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
                 <FaHome className="text-brown-800 text-4xl " />
               </div>
-              <h5>Home</h5>
+              <h5   className= "text-center">Home</h5>
             </div>
 
-            {/* service-one car-service */}
-            <div className="service-one">
+            {/* service-one    w-20 car-service */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
                 <FaCar className="text-yellow-800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Car services</h5>
             </div>
 
-            {/* service-one  Automotive services */}
-            <div className="service-one">
+            {/* service-one    w-20  pet services */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-yellow-800 text-4xl " />
+                <MdOutlinePets  className="text-red-400 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center"   >Pet services</h5>
             </div>
 
-            {/* service-one creative and Artistic  */}
-            <div className="service-one">
+            {/* service-one    w-20 health service  */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+                <TbRibbonHealth className="text-blue-900 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Health service</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 business service */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+              <FaBusinessTime  className="text-red-800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Business Service</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 children care */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+              <FaChildren  className="text-pink-400 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Children care</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20  real-estate */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+              <FaBuilding className="text-gray-800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Real State</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 security service */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+                <MdSecurity className="text-red-800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Security services</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 technology services */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+                <FaComputer className="text-black text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Technology services</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 financial service */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+                <FcMoneyTransfer className="text-red-800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Financial service</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 cleaning-seervices */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+              <MdCleaningServices className="text-red-500 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Cleaning</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one  teravel  w-20 */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+                <GiMountaintop  className="text-blue-800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">teravel</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+                <GrYoga className="text-red-800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Yoga</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+                <MdFamilyRestroom 
+                className="text--800 text-4xl " />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Restaurants</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
+            {/* service-one    w-20 */}
+            <div className="service-one    w-20">
               <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
+              <MdOutlineFaceRetouchingNatural 
+              className="text-red-800 text-4xl" />
               </div>
-              <h5>Restaurants</h5>
+              <h5   className= "text-center">Beauty services</h5>
             </div>
 
-            {/* service-one */}
-            <div className="service-one">
-              <div className="border-2 flex justify-center px-4 py-6 rounded-2xl">
-                <FaPlateWheat className="text-red-800 text-4xl " />
-              </div>
-              <h5>Restaurants</h5>
-            </div>
+           
 
          
          
