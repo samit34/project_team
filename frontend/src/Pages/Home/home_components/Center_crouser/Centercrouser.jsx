135e27026012516dc1113118558ba1d4ac80fd79
curtainscalculator-darius <sehajk.001@gmail>
2025-03-13 09:18:18 +0530

    done three  section

--- frontend/src/Pages/Home/home_components/Center_crouser/Centercrouser.jsx
+++ frontend/src/Pages/Home/home_components/Center_crouser/Centercrouser.jsx
@@ -2,22 +2,41 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import carrepair from '/assets/imageBanner/carrepair.jpg'
+import carwash from '/assets/imageBanner/carwash.jpg';
+import cartow from '/assets/imageBanner/cartow.jpg';
+import carinspect from '/assets/imageBanner/carinspect.jpg';
+import cartyre from '/assets/imageBanner/cartyre.jpg';
+import carrental from "/assets/imageBanner/carrental.jpg";
+import { IoIosStar } from "react-icons/io";
+import { MdCurrencyRupee } from "react-icons/md";
+import Heading from "../../../../Components/heading/Heading.jsx";
 
-import "./Centercrouser";
-import Heading from "../../../../Components/heading/Heading";
+import './centercrouser.css';
+const data = [
+  { autoservice: "car repair and maintenance", rating: "4.5", price: "350", img: carrepair },
+  { autoservice: "Mobile car washing and detailing", rating: "4.5", price: "250", img: carwash },
+  { autoservice: "towing services", rating: "4.5", price: "500", img: cartow },
+  { autoservice: "car inspection and diagnostic", rating: "4.5", price: "400", img: carinspect },
+  { autoservice: "tire changes and repairs", rating: "4.5", price: "150", img: cartyre },
+  { autoservice: "car rental and chauffeur services", rating: "4.5", price: "999", img: carrental },
+
+];
 
 const Centercrouser = () => {
+
+
   const settings = {
     dots: false, // Shows navigation dots below the slider
     infinite: true, // Enables infinite looping
     speed: 7000, // Transition duration (increase for slower movement)
     slidesToShow: 4, // Number of slides visible at once
-    // slidesToScroll: 1, // Number of slides to scroll per transition
+    slidesToScroll: 1, // Number of slides to scroll per transition
     autoplay: true,          // Enables auto-play
     autoplaySpeed: 0, // No delay between transitions
     cssEase: "linear", // Linear easing for smooth, continuous motion
     arrows: false, // Hides next/previous buttons
-    pauseOnHover: true, // Continues sliding even when hovered
+    pauseOnHover: true,// Continues sliding even when hovered
     responsive: [
       {
         breakpoint: 1024,
@@ -48,247 +67,68 @@ const Centercrouser = () => {
 
   return (
     <>
-     <Heading props="Automation Services"/>
-      <div className="container max-w-6xl mx-auto">
-        <div className="slider-container">
-          <Slider {...settings}>
-            <div className="center-box-one">
-              <div class=" bg-white border border-gray-200 rounded-lg shadow-md">
-                <a href="#">
-                  <img
-                    class="rounded-t-lg"
-                    src="./assets/plumber.jpeg"
-                    alt=""
-                  />
-                </a>
-                <div class="p-5">
-                  <a href="#">
-                    <p class="mb-2 tracking-tight text-gray-900">
-                      this that services
-                    </p>
-                  </a>
+      <section className="automotion_service ">
+        <Heading props="Automation Services" />
+        <div className="container max-w-6xl mx-auto p-2 sm:p-0">
+          <div className="slider-container-auto">
+            <Slider {...settings}>
+              {data.map((ser, index) => (
+                <div key={index} className="center-auto-one">
+                  <div class=" bg-white border border-gray-200 rounded-lg shadow-md">
+                    <div className=" for-imgs">
+                      {/*  relative flex  text-center align-middle justify-center items-center  overflow-hidden  */}
+                      <a href="#">
+                        <div className="for-img">
+                          <img
+                            class="rounded-t-lg object-cover w-full h-48 bg-center"
+                            src={ser.img}
+                            alt=""
+                          />
+                          <h5 className="bg-gradient-to-r from-[#FFEDFA] to-[#FFB200] bg-clip-text text-transparent capitalize pb-3">click to explore</h5>
+                        </div>
+                      </a>
+                    </div>
+                    <div class="py-2 px-4">
+                      <div className="my-2  h-[7vh]">
+                        <a href="#">
+                          {/* text-[#f4ade2]  */}
+                          <p class="mb-2 text-gray-500 text-base font-medium capitalize">
+                            {ser.autoservice}
+                          </p>
+                        </a>
 
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      rating
-                    </p>
-                  </a>
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      price
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="center-box-two">
-            <div class=" bg-white border border-gray-200 rounded-lg shadow-md">
-                <a href="#">
-                  <img
-                    class="rounded-t-lg"
-                    src="./assets/plumber.jpeg"
-                    alt=""
-                  />
-                </a>
-                <div class="p-5">
-                  <a href="#">
-                    <p class="mb-2 tracking-tight text-gray-900">
-                      this that services
-                    </p>
-                  </a>
+                      </div>
+                      <div className="flex justify-between mt-3 ">
+                        <div className="rating">
+                          <a href="#">
+                            <p class="mb-2  tracking-tight text-gray-600 flex gap-1 items-center justify-center">
+                              <IoIosStar className="w-4 h-4 text-amber-400" />{ser.rating}
+                            </p>
+                          </a>
+                        </div>
+                        <div className="price flex flex-col gap-px">
+                          <a href="#">
+                            <p class="mb-2  tracking-tight text-gray-600 flex gap-px items-center justify-center">
+                              <MdCurrencyRupee className="w-4 h-4 text-black" /> {ser.price}
+                            </p>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      rating
-                    </p>
-                  </a>
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      price
-                    </p>
-                  </a>
                 </div>
-              </div>
-            </div>
-            <div className="center-box-three">
-            <div class=" bg-white border border-gray-200 rounded-lg shadow-md">
-                <a href="#">
-                  <img
-                    class="rounded-t-lg"
-                    src="./assets/plumber.jpeg"
-                    alt=""
-                  />
-                </a>
-                <div class="p-5">
-                  <a href="#">
-                    <p class="mb-2 tracking-tight text-gray-900">
-                      this that services
-                    </p>
-                  </a>
+              ))}
 
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      rating
-                    </p>
-                  </a>
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      price
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="center-box-four">
-            <div class=" bg-white border border-gray-200 rounded-lg shadow-md">
-                <a href="#">
-                  <img
-                    class="rounded-t-lg"
-                    src="./assets/plumber.jpeg"
-                    alt=""
-                  />
-                </a>
-                <div class="p-5">
-                  <a href="#">
-                    <p class="mb-2 tracking-tight text-gray-900">
-                      this that services
-                    </p>
-                  </a>
+            </Slider>
+          </div>
 
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      rating
-                    </p>
-                  </a>
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      price
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="center-box-five">
-            <div class=" bg-white border border-gray-200 rounded-lg shadow-md">
-                <a href="#">
-                  <img
-                    class="rounded-t-lg"
-                    src="./assets/plumber.jpeg"
-                    alt=""
-                  />
-                </a>
-                <div class="p-5">
-                  <a href="#">
-                    <p class="mb-2 tracking-tight text-gray-900">
-                      this that services
-                    </p>
-                  </a>
-
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      rating
-                    </p>
-                  </a>
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      price
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="center-box-six">
-            <div class=" bg-white border border-gray-200 rounded-lg shadow-md">
-                <a href="#">
-                  <img
-                    class="rounded-t-lg"
-                    src="./assets/plumber.jpeg"
-                    alt=""
-                  />
-                </a>
-                <div class="p-5">
-                  <a href="#">
-                    <p class="mb-2 tracking-tight text-gray-900">
-                      this that services
-                    </p>
-                  </a>
-
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      rating
-                    </p>
-                  </a>
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      price
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="center-box-seven">
-            <div class=" bg-white border border-gray-200 rounded-lg shadow-sm">
-                <a href="#">
-                  <img
-                    class="rounded-t-lg"
-                    src="./assets/plumber.jpeg"
-                    alt=""
-                  />
-                </a>
-                <div class="p-5">
-                  <a href="#">
-                    <p class="mb-2 tracking-tight text-gray-900">
-                      this that services
-                    </p>
-                  </a>
-
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      rating
-                    </p>
-                  </a>
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      price
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="c-box-eight">
-            <div class=" bg-white border border-gray-200 rounded-lg shadow-md">
-                <a href="#">
-                  <img
-                    class="rounded-t-lg"
-                    src="./assets/plumber.jpeg"
-                    alt=""
-                  />
-                </a>
-                <div class="p-5">
-                  <a href="#">
-                    <p class="mb-2 tracking-tight text-gray-900">
-                      this that services
-                    </p>
-                  </a>
-
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      rating
-                    </p>
-                  </a>
-                  <a href="#">
-                    <p class="mb-2  tracking-tight text-gray-900">
-                      price
-                    </p>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </Slider>
         </div>
-      </div>
+
+      </section>
     </>
   );
 };
 
 export default Centercrouser;
+
--- frontend/src/Pages/Home/home_components/additionalServices/AddServices.jsx
+++ frontend/src/Pages/Home/home_components/additionalServices/AddServices.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import Heading from '../../../../Components/heading/Heading'
-// import img1 from '/assets/addser1.png';
+import { MdCurrencyRupee } from "react-icons/md";
+import Button from '../../../../Components/button/Button';
+import img1 from "/assets/addser1.png";
+import img2 from "/assets/addser5.png" ;
+import img3 from "/assets/addser3.png";
+import img4 from "/assets/addser1.png";
+import img5 from "/assets/addser6.png";
+import img6 from "/assets/addser7.png";
+import img7 from "/assets/addser8.png";
+import img8 from "/assets/addser1.png";
+const data =[
+    {name:"Home Services",img:img1 , price:"399",link:"#"},
+    {name:"Automotive Services",img:img2, price:"499",link:"#"},
+    {name:"Business Services",img:img3, price:"699",link:"#"},
+    {name:"Pet Services",img:img4, price:"399",link:"#"},
+    {name:"Event And Entertainment Services",img:img5, price:"399",link:"#"},
+    {name:"Transport and Logistics Services",img:img6, price:"499",link:"#"},
+    {name:"Legal and Financial Services",img:img7, price:"499",link:"#"},
+    {name:"Childcare and Family Services",img:img8, price:"399",link:"#"}
+    
 
-import Button from '../../../../Components/button/Button'
+]
 const AddServices = () => {
     return (
         <>
@@ -10,167 +29,32 @@ const AddServices = () => {
                 <Heading props='Most Book Services' />
                 <div className="adds-container ">
                     <div className="max-w-6xl mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                        <div className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
+                        {data.map((ser,index)=>(
+
+                    
+                        <div key={index} className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
                             <div className="flex flex-row space-x-14 sm:space-x-10">
                                 <div className="w-1/2">
                                     <div className="w-16 h-16">
-                                    <img src="./assets/addser1.png" alt="" className='w-16 h-16 img-fluid' />
+                                    <img src={ser.img} alt="" className='w-16 h-16 img-fluid' />
                                     </div>
                                 </div>
                                 <div className="w-1/2 pl-2">
-                                    <p className='text-[12px]'>FROM $399</p>
+                                    <p className='text-[12px] flex items-center'><span className=' pr-1 text-[#fd7ed9] font-semibold'>FROM </span> <MdCurrencyRupee className='w-3 h-3 text-black'/>{ser.price}</p>
                                 </div>
                             </div>
 
                             <div className="w-full flex flex-col">
                                 <div className="heading-h1 w-full">
-                                    <h2 className='text-md md:text-xl font-semibold pb-3  break-all'>Home Services</h2>
-                                    <Button props='Learn More' />
+                                    <h2 className='text-base md:text-lg pb-3 break-words text-gray-500 font-medium capitalize'>{ser.name}</h2>
+                                    <button class=" text-sm font-semibold py-1 bg-gradient-to-r from-[#f684d6] to-[#FFB200] bg-clip-text text-transparent"> Explore more →</button>
                                 </div>
                                 
                             </div>
 
                         </div>
-                        <div className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
-                            <div className="flex flex-row space-x-14 sm:space-x-10">
-                                <div className="w-1/2">
-                                <div className="w-16 h-16">
-                                    <img src="./assets/addser5.png" alt="" className='w-16 h-16 img-fluid' />
-                                    </div>
-                                </div>
-                                <div className="w-1/2 pl-2">
-                                    <p className='text-[12px]'>FROM $499</p>
-                                </div>
-                            </div>
-
-                            <div className="w-full flex flex-col">
-                                <div className="heading-h1 ">
-                                    <h2 className='text-md md:text-xl font-semibold pb-3  break-all'>Automotive Services</h2>
-                                    <Button props='Learn More' />
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
-                            <div className="flex flex-row space-x-14 sm:space-x-10">
-                                <div className="w-1/2">
-                                <div className="w-16 h-16">
-                                    <img src="./assets/addser3.png" alt="" className='w-16 h-16 img-fluid' />
-                                    </div>
-                                </div>
-                                <div className="w-1/2 pl-2">
-                                    <p className='text-[12px]'>FROM $699</p>
-                                </div>
-                            </div>
-
-                            <div className="w-full flex flex-col">
-                                <div className="heading-h1 ">
-                                    <h2 className='text-md md:text-xl font-semibold pb-3  break-all'>Business Services</h2>
-                                    <Button props='Learn More' />
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
-                            <div className="flex flex-row space-x-14 sm:space-x-10">
-                                <div className="w-1/2">
-                                <div className="w-16 h-16">
-                                    <img src="./assets/addser1.png" alt="" className='w-16 h-16 img-fluid' />
-                                    </div>
-                                </div>
-                                <div className="w-1/2 pl-2">
-                                    <p className='text-[12px]'>FROM $399</p>
-                                </div>
-                            </div>
-                            
-                            <div className="w-full flex flex-col">
-                                <div className="heading-h1 ">
-                                    <h2 className='text-md md:text-xl font-semibold pb-3  break-all'>Pet Services</h2>
-                                    <Button props='Learn More' />
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
-                            <div className="flex flex-row space-x-14 sm:space-x-10">
-                                <div className="w-1/2">
-                                <div className="w-16 h-16">
-                                    <img src="./assets/addser6.png" alt="" className='w-16 h-16 img-fluid' />
-                                    </div>
-                                </div>
-                                <div className="w-1/2 pl-2">
-                                    <p className='text-[12px]'>FROM $399</p>
-                                </div>
-                            </div>
-
-                            <div className="w-full flex flex-col">
-                                <div className="heading-h1 ">
-                                    <h2 className='text-md md:text-xl font-semibold pb-3 break-all'>Event And Entertainment Services</h2>
-                                    <Button props='Learn More' />
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
-                            <div className="flex flex-row space-x-14 sm:space-x-10">
-                                <div className="w-1/2">
-                                <div className="w-16 h-16">
-                                    <img src="./assets/addser7.png" alt="" className='w-16 h-16 img-fluid' />
-                                    </div>
-                                </div>
-                                <div className="w-1/2 pl-2">
-                                    <p className='text-[12px]'>FROM $399</p>
-                                </div>
-                            </div>
-
-                            <div className="w-full flex flex-col">
-                                <div className="heading-h1 ">
-                                    <h2 className='text-md md:text-xl font-semibold pb-3 break-all '>Transport and Logistics Services</h2>
-                                    <Button props='Learn More' />
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
-                            <div className="flex flex-row space-x-14 sm:space-x-10">
-                                <div className="w-1/2">
-                                <div className="w-16 h-16">
-                                    <img src="./assets/addser8.png" alt="" className='w-16 h-16 img-fluid' />
-                                    </div>
-                                </div>
-                                <div className="w-1/2 pl-2">
-                                    <p className='text-[12px]'>FROM $399</p>
-                                </div>
-                            </div>
-
-                            <div className="w-full flex flex-col">
-                                <div className="heading-h1 ">
-                                    <h2 className='text-md md:text-xl font-semibold pb-3 break-all'>Legal and Financial Services</h2>
-                                    <Button props='Learn More' />
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="rounded-lg bg-white p-6 shadow-md flex flex-col  gap-6">
-                            <div className="flex flex-row space-x-14 sm:space-x-10">
-                                <div className="w-1/2">
-                                <div className="w-16 h-16">
-                                    <img src="./assets/addser1.png" alt="" className='w-16 h-16 img-fluid' />
-                                    </div>
-                                </div>
-                                <div className="w-1/2 pl-2">
-                                    <p className='text-[12px]'>FROM $399</p>
-                                </div>
-                            </div>
-
-                            <div className="w-full flex flex-col">
-                                <div className="heading-h1 ">
-                                    <h2 className='text-md md:text-xl font-semibold pb-3  break-all'>Childcare and Family Services</h2>
-                                    <Button props='Learn More' />
-                                </div>
-                            </div>
-
-                        </div>
+                            ))}
+                       
                     </div>
                 </div>
             </section>
